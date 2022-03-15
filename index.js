3a2a9df65e1d0d2da20c4e7e20bce1f06c5a42a7
Janice Chan <janice.chan1288@gmail.com>
2022-03-15 14:17:13 -0400

    commit tree 25267ac7fc90096636aabd34fec082dad3bafdcd

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+return Math.abs(pickupLocation - 42);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+   return distanceFromHqInBlocks(pickupLocation)*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((start-destination)*264);
+}
+
+function calculatesFarePrice(start, destination) {
+    let feet = Math.abs((start-destination)*264);
+    if(feet <= 400) {
+        return 0;
+    } else if(400 < feet && feet <= 2000) {
+        return ((feet - 400) * .02);
+    } else if(feet > 2000 && feet < 2500) {
+        return 25;
+    } else if(feet > 2500) {
+     return 'cannot travel that far';
+   } 
+}
